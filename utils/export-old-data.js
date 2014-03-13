88c5ec08b82b2d85a1533673c2fef496fb388dc9
John Resig <jeresig@gmail.com>
2014-03-13 12:13:45 -0400

    Add me_id to export-old-data.js.

--- utils/export-old-data.js
+++ utils/export-old-data.js
@@ -18,6 +18,7 @@ var processSource = function(source, callback) {
             this.queue({
                 id: image._id,
                 image_id: image._id + ".jpg",
+                me_id: image.source + "/" + image.imageName,
                 source: image.source,
                 source_image: image.imageURL,
                 image_file: image.imageName + ".jpg",
