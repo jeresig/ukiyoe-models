7058a74180935ec62a53ef499bc4a83b683d4552
John Resig <jeresig@gmail.com>
2014-04-01 21:21:10 -0400

    Add loading in NDLNA bios based upon other bios.

--- utils/ndlna-bio-gen.js
+++ utils/ndlna-bio-gen.js
@@ -3,20 +3,30 @@ var ndlna = require("ndlna");
 
 var ukiyoe = require("../");
 
+var source = process.argv[2];
 var names = {};
 
 var ExtractedImage = ukiyoe.db.model("ExtractedImage");
 var Bio = ukiyoe.db.model("Bio");
 
-var queue = async.queue(function(artist, callback) {
-    var kanji = ukiyoe.romajiName.parseName(artist.kanji);
+var queue = async.queue(function(query, callback) {
+    var name = ukiyoe.romajiName.parseName(query);
 
-    if (!kanji.surname_kanji) {
+    if (name.given_kanji && !name.surname_kanji ||
+            name.given && !name.surname || name.locale !== "ja") {
         process.nextTick(callback);
         return;
     }
 
-    ndlna.searchByName(kanji.kanji, function(err, search) {
+    query = name.kanji || [
+        name.surname,
+        name.given,
+        name.generation || ""
+    ].join(" ").trim();
+
+    console.log("Searching", query);
+
+    ndlna.searchByName(query, function(err, search) {
         var filtered = search.results.filter(function(record) {
             return /1[678]\d\d/.test(record.label);
         });
@@ -31,11 +41,16 @@ var queue = async.queue(function(artist, callback) {
 
         if (match) {
             match.load(function() {
-                match._id = "ndlna/" + match.id;
-                match.source = "ndlna";
-                var bio = new Bio(match);
-                console.log("Adding", bio.name.original);
-                bio.save(callback);
+                if (match.name.kanji && match.name.locale === "ja") {
+                    match._id = "ndlna/" + match.id;
+                    match.source = "ndlna";
+                    var bio = new Bio(match);
+                    console.log("Adding", bio.name.original);
+                    //bio.save(callback);
+                    callback();
+                } else {
+                    callback();
+                }
             });
         } else {
             process.nextTick(callback);
@@ -43,34 +58,52 @@ var queue = async.queue(function(artist, callback) {
     });
 }, 1);
 
-var processArtist = function(artist) {
-    if (!(artist.kanji in names)) {
-        names[artist.kanji] = 0;
-        queue.push(artist);
+var processArtist = function(query) {
+    if (!query) {
+        return;
     }
-    names[artist.kanji] += 1;
+
+    if (!(query in names)) {
+        names[query] = 0;
+        queue.push(query);
+    }
+    names[query] += 1;
 };
 
 ukiyoe.init(function() {
     ndlna.init(function() {
+        if (source) {
+            var query = {source: source};
+            Bio.find(query).stream()
+                .on("data", function(bio) {
+                    //if (!bio.name.kanji) {
+                        processArtist(bio.name.kanji || bio.name.name);
+                    //}
+                })
+                .on("error", function(err) {
+                    console.error(err);
+                })
+                .on("close", function() {
+                    queue.drain = function() {
+                        console.log("DONE");
+                        process.exit(0);
+                    };
+                });
+            return;
+        }
+
         var query = {"artists.name": null, "artists.kanji": {$ne: null}};
         ExtractedImage.find(query).stream()
             .on("data", function(image) {
-                image.artists.forEach(processArtist);
+                image.artists.forEach(function(artist) {
+                    processArtist(artist.kanji);
+                });
             })
             .on("error", function(err) {
                 console.error(err);
             })
             .on("close", function() {
                 queue.drain = function() {
-                    /*
-                    Object.keys(names).sort(function(a, b) {
-                        return names[b] - names[a];
-                    }).forEach(function(name) {
-                        console.log(ukiyoe.romajiName.parseName(name).kanji);
-                    });
-                    */
-
                     console.log("DONE");
                     process.exit(0);
                 };
