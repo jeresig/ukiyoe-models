32940802b965cd3ce487123c4079a3aae7f1cdb1
John Resig <jeresig@gmail.com>
2014-06-25 23:57:23 -0400

    Create script for importing old uploads into the new system.

---
+++ utils/import-uploads.js
@@ -0,0 +1,35 @@
+var async = require("async");
+var readline = require("readline");
+
+var ukiyoe = require("../");
+
+var Upload = ukiyoe.db.model("Upload");
+
+ukiyoe.init(function() {
+    var rl = readline.createInterface({
+        input: process.stdin,
+        output: process.stdout
+    });
+
+    rl.on("line", function(fileName) {
+        rl.pause();
+
+        fileName = fileName.replace(/\.jpg$/, "");
+
+        var Upload = new Upload({
+            _id: "uploads/" + fileName,
+            imageID: "uploads/" + fileName,
+            imageName: fileName,
+            source: "uploads"
+        });
+
+        upload.save(function() {
+            rl.resume();
+        });
+    });
+
+    rl.on("close", function() {
+        console.log("DONE");
+        process.exit(0);
+    });
+});
