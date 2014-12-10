56a6247b9b7a5c70b4fd8761079cf13a572c6058
John Resig <jeresig@gmail.com>
2014-12-09 20:55:48 -0500

    Abstract out the readline-centric functionality into a separate utility.

--- utils/upgrade-extracted.js
+++ utils/upgrade-extracted.js
@@ -1,55 +1,11 @@
-var readline = require("readline");
-
 var async = require("async");
 
 var ukiyoe = require("../");
+var readline = require("./readline-utils");
 
 var ExtractedImage = ukiyoe.db.model("ExtractedImage");
 
-var rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
-});
-
-var formatName = function(name) {
-    return name.name ?
-        name.name + (name.kanji ? " (" + name.kanji  + ")" : "") :
-        name.kanji;
-};
-
-var formatDate = function(range) {
-    return range ?
-        (range.start || "") + " - " + (range.end || "") : "";
-};
-
-var renderArtist = function(artist, i) {
-    var parts = [formatName(artist.name)];
-
-    if (artist.life) {
-        parts.push("Life: " + formatDate(artist.life));
-    }
-
-    if (artist.active) {
-        parts.push("Active: " + formatDate(artist.active));
-    }
-
-    artist.aliases.forEach(function(name) {
-        parts.push(" - " + formatName(name));
-    });
-
-    if (artist.bios) {
-        parts.push("Bios (" + artist._id + "): ");
-
-        artist.bios.forEach(function(bio) {
-            parts.push(" - " + bio._id + ": " + bio.url);
-        });
-    } else {
-        parts.push(" - " + artist._id + ": " + artist.url);
-    }
-
-    console.log((i >= 0 ? (i + 1) + ") " : "   ") +
-        parts.map(function(l){return "   " + l;}).join("\n").trim());
-};
+var rl = readline.connect();
 
 var choices = {};
 var names = {};
@@ -76,8 +32,8 @@ var processQueue = function() {
                         return callback(choices[original]);
                     }
 
-                    renderArtist(bio, -1);
-                    possibleArtists.forEach(renderArtist);
+                    readline.renderArtist(bio, -1);
+                    possibleArtists.forEach(readline.renderArtist.bind(readline));
 
                     rl.question("Which artist? [0 for None] ", function(answer) {
                         if (answer) {
