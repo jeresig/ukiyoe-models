f2c4a7259723403d56b7ddfac840a23e0d5e688f
John Resig <jeresig@gmail.com>
2015-02-26 23:40:39 -0500

    Parse CSV file.

--- utils/import-csv.js
+++ utils/import-csv.js
@@ -1,4 +1,5 @@
 var fs = require("fs");
+var csv = require("csv-streamify");
 var path = require("path");
 var async = require("async");
 var yr = require("yearrange");
@@ -31,59 +32,46 @@ var Artwork = ukiyoe.db.model("Artwork");
 var Image = ukiyoe.db.model("Image");
 var ExtractedImage = ukiyoe.db.model("ExtractedImage");
 
-var files = process.argv.slice(2);
+var processItem = function(data, callback) {
+    console.log("Saving:", data.source, data.source_id);
 
-var processFile = function(file, callback) {
-    console.log("Processing:", file);
+    var imageName = data.image_file.replace(/.jpg$/, "");
 
-    var datas = JSON.parse(fs.readFileSync(file, "utf8"));
-
-    nameCache = {};
-
-    console.log("Removing old extracted images...");
-
-    ExtractedImage.remove({source: datas[0].source}, function(err) {
-        async.eachLimit(datas, 10, function(data, callback) {
-            if (!data) {
-                return callback();
-            }
-
-            console.log("Saving:", data.source, data.source_id);
-
-            var imageName = data.image_file.replace(/.jpg$/, "");
-
-            ExtractedImage.create({
-                _id: data.source + "/" + imageName,
-                source: data.source,
-                modified: Date.now(),
-                extract: ["", data.source_id],
-                extracted: true,
-                imageURL: data.source_image,
-                imageName: imageName,
-                pageID: data.source_id,
-                url: data.source_url,
-                lang: args.lang,
-                artists: data.artist,
-                title: data.title,
-                description: data.description,
-                dateCreated: data.date ? yr.parse(data.date) : null
-            }, function(err) {
-                if (err) {
-                    // Ignore the error (could be a duplicate key error)
-                    console.error(err);
-                }
-                callback();
-            });
-        }, callback);
+    ExtractedImage.create({
+        _id: data.source + "/" + imageName,
+        source: data.source,
+        modified: Date.now(),
+        extract: ["", data.source_id],
+        extracted: true,
+        imageURL: data.source_image,
+        imageName: imageName,
+        pageID: data.source_id,
+        url: data.source_url,
+        lang: args.lang,
+        artists: data.artist,
+        title: data.title,
+        description: data.description,
+        dateCreated: data.date ? yr.parse(data.date) : null
+    }, function(err) {
+        if (err) {
+            // Ignore the error (could be a duplicate key error)
+            console.error(err);
+        }
+        callback();
     });
 };
 
 ukiyoe.init(function() {
-    async.eachLimit(files, 1, processFile, function(err) {
-        if (err) {
-            console.error(err);
-        }
-        console.log("DONE");
-        process.exit(0);
-    });
+    fs.createReadStream(args.csvFile)
+        .pipe(csv({objectMode: true, columns: true}))
+        .on("data", function(data) {
+            this.pause();
+            processItem(data, function() {
+                this.resume();
+            }.bind(this));
+        })
+        .on("close", function() {
+            console.log("DONE");
+            process.exit(0);
+        });
 });
