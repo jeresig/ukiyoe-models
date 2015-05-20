ce02bfac86ccccb3df422c0b8403b41f22b0e516
John Resig <jeresig@gmail.com>
2015-05-19 20:26:48 -0400

    Generate a JSON file to be used for mapping images to their correct location.

--- utils/change-image-md5s.js
+++ utils/change-image-md5s.js
@@ -11,17 +11,39 @@ argparser.addArgument(["imageMap"], {
     help: "A file that has a MD5 to image file mapping."
 });
 
+argparser.addArgument(["jsonImageMap"], {
+    help: "A file that a JSON image map will be written to."
+});
+
 var args = argparser.parseArgs();
+
+var jsonMap = {};
 
 fs.createReadStream(args.imageMap)
     .pipe(es.split())
     .on("data", function(data) {
-        this.pause();
+        if (!data) {
+            return;
+        }
+        //this.pause();
 
-        var parts = data.split(/\s/);
+        var parts = data.split(/\s+/);
         var md5 = parts[0];
         var file = parts[1];
+        var path = file.split("/");
+        var source = path[0];
+        var fileName = path[2].replace(".jpg", "");
+        var id = source + "/" + fileName;
 
+        jsonMap[id] = md5;
 
-        this.resume();
+        //this.resume();
+    })
+    .on("close", function() {
+        var data = JSON.stringify(jsonMap);
+
+        fs.writeFile(args.jsonImageMap, data, function() {
+            console.log("DONE");
+            process.exit(0);
+        });
     });
